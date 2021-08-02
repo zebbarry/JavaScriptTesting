a081dff9a980ef0b08c263da294664f66883f6bf
Zeb Barry <zba24@uclive.ac.nz>
2021-08-02 22:21:00 +1200

    Count number of visits

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const { readFile } = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
 
 const app = express();
+const count_file_path = './count.txt'
+let view_count = 0
+
+readFile(count_file_path, 'utf-8').then((data) => {
+    view_count += parseInt(data);
+    console.log('Read count as #' + view_count);
+});
+
+const incrementViewCount = async () => {
+    view_count++;
+    await writeFile(count_file_path, view_count.toString(), 'utf-8')
+}
 
 app.get('/', async (request, response) => {
-    console.log('Request received!')
+    await incrementViewCount()
+        .then(() => { console.log('Request received! #' + view_count) });
+
     await readFile('./sandbox.html', 'utf-8')
         .then((html) => {
             response.send(html);
