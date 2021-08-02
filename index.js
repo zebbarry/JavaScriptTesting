b6fd758d7513b138ef62c5040c2ea0a288b62a36
Zeb Barry <zba24@uclive.ac.nz>
2021-08-02 21:09:12 +1200

    Init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+
+const app = express();
+
+app.get('/', async (request, response) => {
+    console.log('Request received!')
+    await readFile('./sandbox.html', 'utf-8')
+        .then((html) => {
+            response.send(html)
+        }).catch(() => {
+            console.error('Something went wrong?')
+            response.status(500).send("This wasn't meant to happen?!")
+        })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => { console.log('App available on http://localhost:' + port) })
