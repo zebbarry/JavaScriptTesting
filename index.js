ba094b9d0abf9917bbb63d2e0f5611592fef3a7e
Zeb Barry <zba24@uclive.ac.nz>
2021-08-02 21:13:58 +1200

    Add semicolons

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.get('/', async (request, response) => {
     console.log('Request received!')
     await readFile('./sandbox.html', 'utf-8')
         .then((html) => {
-            response.send(html)
+            response.send(html);
         }).catch(() => {
-            console.error('Something went wrong?')
-            response.status(500).send("This wasn't meant to happen?!")
-        })
-})
+            console.error('Something went wrong?');
+            response.status(500).send("This wasn't meant to happen?!");
+        });
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => { console.log('App available on http://localhost:' + port) })
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log('App available on http://localhost:' + port) });
