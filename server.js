583cd989f306b910d3d850f4410697a44b4c7097
Zeb Barry <zba24@uclive.ac.nz>
2021-08-03 19:40:14 +1200

    Improve error handling and add count to html

--- server.js
+++ server.js
@@ -2,31 +2,33 @@ const express = require('express');
 const { readFile, writeFile } = require('fs').promises;
 
 const app = express();
-const count_file_path = './count.txt'
-let view_count = 0
+const count_file_path = './count.txt';
+const port = process.env.PORT || 3000;
+let view_count = 0;
 
 readFile(count_file_path, 'utf-8').then((data) => {
     view_count += parseInt(data);
     console.log('Read count as #' + view_count);
-});
+}).catch(() => {
+    console.log('Could not find view count file ' + count_file_path);
+})
 
 const incrementViewCount = async () => {
     view_count++;
-    await writeFile(count_file_path, view_count.toString(), 'utf-8')
-}
+    await writeFile(count_file_path, view_count.toString(), 'utf-8');
+};
 
 app.get('/', async (request, response) => {
     await incrementViewCount()
-        .then(() => { console.log('Request received! #' + view_count) });
+        .then(() => { console.log(`Request received! #${view_count}`) });
 
-    await readFile('./sandbox.html', 'utf-8')
+    await readFile('./index.html', 'utf-8')
         .then((html) => {
-            response.send(html);
+            response.send(html.replace(`id="count">0<`,`id="count">${view_count}<`));
         }).catch(() => {
             console.error('Something went wrong?');
             response.status(500).send("This wasn't meant to happen?!");
         });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log('App available on http://localhost:' + port) });
+app.listen(port, () => { console.log(`App available on http://localhost:${port}`) });
