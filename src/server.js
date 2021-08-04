07060c63dd8af002139a06f6afd90c1c414a0aa3
Zeb Barry <zba24@uclive.ac.nz>
2021-08-04 22:21:35 +1200

    Track current connections

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const count_file_path = (process.env.SHARED_DIR || 'lib') + '/count.txt';
 const html_path = 'src/index.html';
 const port = process.env.PORT || 3000;
 let view_count = 0;
+let connect_count = 0;
 
 readFile(count_file_path, 'utf-8').then((data) => {
     view_count += parseInt(data);
@@ -19,17 +20,24 @@ const incrementViewCount = async () => {
     await writeFile(count_file_path, view_count.toString(), 'utf-8');
 };
 
+const server = app.listen(port, () => { console.log(`App available on http://localhost:${port}`) })
+    .on('connection', (socket) => {
+        connect_count++;
+        socket.on('close', () => {
+            connect_count--;
+        })
+    });
+
 app.get('/', async (request, response) => {
     await incrementViewCount()
         .then(() => { console.log(`Request received! #${view_count}`) });
 
     await readFile(html_path, 'utf-8')
         .then((html) => {
-            response.send(html.replace('id="count">0<',`id="count">${view_count}<`));
+            response.send(html.replace('id="view_count">0<',`id="view_count">${view_count}<`)
+                .replace('id="connect_count">0<',`id="connect_count">${connect_count}<`));
         }).catch(() => {
             console.error('Something went wrong?');
             response.status(500).send("This wasn't meant to happen?!");
         });
 });
-
-app.listen(port, () => { console.log(`App available on http://localhost:${port}`) });
