a760475d313955dd8b2148caff1eb0745946d236
Zeb Barry <zba24@uclive.ac.nz>
2021-08-06 22:09:44 +1200

    Working realtime socket connections

--- server/index.js
+++ server/index.js
@@ -1,25 +1,16 @@
-const express = require('express');
-const io = require('socket.io');
 const { readFile, writeFile } = require('fs').promises;
+const http = require('http').createServer();
+const io = require('socket.io')(http, {
+    cors: { origin: '*'}
+});
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 8080;
 const count_file_path = (process.env.SHARED_DIR ?? 'lib') + '/count.txt';
 const html_path = 'src/index.html';
 const indexjs_path = 'src/index.js';
 
 let view_count = 0;
 let connect_count = 0;
-
-const app = express();
-const server = io(app.listen(port, () => console.log(`App available on http://localhost:${port}/app`)), {
-    cors: { origin: '*'}
-});
-
-server.on('connection', socket => {
-    connect_count++;
-    console.log('User Connected!');
-    io.emit('connect count', connect_count)
-});
 
 readFile(count_file_path, 'utf-8').then((data) => {
     view_count += parseInt(data);
@@ -33,32 +24,31 @@ const incrementViewCount = async () => {
     await writeFile(count_file_path, view_count.toString(), 'utf-8');
 };
 
-app.get('/app', async (request, response) => {
-    await incrementViewCount()
-        .then(() => { console.log(`HTML Request received! #${view_count}`) });
-
-    await readFile(html_path, 'utf-8').then(html => {
-        response.send(html.replace('id="view_count">0<',`id="view_count">${view_count}<`)
-            .replace('id="connect_count">0<',`id="connect_count">${connect_count}<`));
-    }).catch(() => {
-        console.error('Something went wrong?');
-        response.status(500).send("This wasn't meant to happen?!");
+io.on('connection', socket => {
+    connect_count++;
+    incrementViewCount();
+    console.log('New Connection!');
+    console.log({view_count, connect_count});
+    io.emit('update', {views: view_count, connections: connect_count});
+    socket.on('disconnect', socket => {
+        connect_count--;
+        console.log('Connection lost...');
+        console.log({view_count, connect_count})
+        io.emit('update', {views: view_count, connections: connect_count});
     });
 });
 
-app.get('/index.js', async (request, response) => {
-    await readFile(indexjs_path, 'utf-8').then(indexjs => response.send(indexjs)).catch(() => {
-        console.error('Something went wrong I think?');
-        response.status(500).send("This probably wasn't meant to happen?!");
-    });
-});
+// app.get('/app', async (request, response) => {
+//     await incrementViewCount()
+//         .then(() => { console.log(`HTML Request received! #${view_count}`) });
 
-app.get('/counts', (request, response) => {
-    const reply = {
-        views: view_count,
-        connections: connect_count
-    }
-    console.log('Count request recieved');
-    console.log(reply)
-    response.send(reply);
-});
+//     await readFile(html_path, 'utf-8').then(html => {
+//         response.send(html.replace('id="view_count">0<',`id="view_count">${view_count}<`)
+//             .replace('id="connect_count">0<',`id="connect_count">${connect_count}<`));
+//     }).catch(() => {
+//         console.error('Something went wrong?');
+//         response.status(500).send("This wasn't meant to happen?!");
+//     });
+// });
+
+http.listen(port, () => console.log(`App available on http://localhost:${port}`));
